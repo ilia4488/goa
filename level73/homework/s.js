40337bca3ca26fb016872ae7a3d7b4d7cde7ae75
ilia4488 <annarichi2021@gmail.com>
2026-06-17 15:13:25 +0400

    new

--- level73/homework/s.js
+++ level73/homework/s.js
@@ -1,1 +1,25 @@
-const form = document.querySelector("form")
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit',(event)=> {
+
+    event.preventDefault();
+
+    const firstName = document.getElementById('firstname').value;
+    const lastName = document.getElementById('lastname').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('msg').value;
+
+    const formData = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        message: message,
+
+    };
+
+    localStorage.setItem('contactFormData', JSON.stringify(formData));
+
+    alert('მონაცემები წარმატებით შეინახა localStorage-ში!');
+    console.log(localStorage.getItem('contactFormData'));
+});
